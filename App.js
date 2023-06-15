43596221156d8b94e00372212c670cfb13d732c4
vipul <vipulmauryaedu@gmail.com>
2023-06-15 16:18:23 +0530

    bug fixed 1

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
-
 import RootNavigator from './src/Navigation/RootNavigator';
 
 const App = () => {
